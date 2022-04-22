a6ff59b513d26bc93fd80d440524146f52ec0106
Michael van der Gulik <mikevdg@gulik.co.nz>
2022-04-22 04:37:25 +0000

    Remember to init() a servlet in the tests.

--- src/test/java/gulik/urad/TestServlet.java
+++ src/test/java/gulik/urad/TestServlet.java
@@ -1,6 +1,9 @@
 package gulik.urad;
 
 import static org.junit.Assert.*;
+
+import javax.servlet.http.HttpServlet;
+
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
@@ -14,12 +17,16 @@ public class TestServlet  {
     @Test
     public void testServlet() throws Exception {
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "/$metadata");
+        request.addParameter("odata-debug", "json");
         MockHttpServletResponse response = new MockHttpServletResponse();
         
         // This doesn't work unfortunately. You still get HTML.
         // request.setContentType("application/json");
 
-        new VegetableServlet().service(request, response);
+
+        HttpServlet servlet = new VegetableServlet();
+        servlet.init(null);
+        servlet.service(request, response);
         response.getWriter().flush();
         String result = response.getContentAsString();
         System.out.println(result);
