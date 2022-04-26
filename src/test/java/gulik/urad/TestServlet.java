ed99cbae12d6dc87739dfc4f8170fa5349585164
Michael van der Gulik <mikevdg@gulik.co.nz>
2022-04-26 01:14:26 +0000

    Add tests that fail.

--- src/test/java/gulik/urad/TestServlet.java
+++ src/test/java/gulik/urad/TestServlet.java
@@ -2,6 +2,9 @@ package gulik.urad;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -13,26 +16,46 @@ import gulik.demo.VegetableServlet;
 import org.junit.Test;
 
 public class TestServlet  {
-    
-    @Test
-    public void testServlet() throws Exception {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "/$metadata");
+
+    /** Fetch that URL and check that the result contains the given string. 
+     * @throws IOException
+     * @throws ServletException */
+    private void checkForResponse(String url, String contains) throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest("GET", url);
         request.addParameter("odata-debug", "json");
         MockHttpServletResponse response = new MockHttpServletResponse();
         
         // This doesn't work unfortunately. You still get HTML.
         // request.setContentType("application/json");
 
-
         HttpServlet sv = new VegetableServlet();
         sv.init(null);
-        
         
         sv.service(request, response);
         response.getWriter().flush();
         String result = response.getContentAsString();
         System.out.println(result);
         
-        assertTrue(result.contains("Vegetable"));
+        assertTrue(result.contains(contains));
+    }
+
+    @Test
+    public void testServlet() throws Exception {
+        checkForResponse("/$metadata", "Vegetable");
+    }
+
+    @Test
+    public void testFruitProperty() throws Exception {
+        checkForResponse("/Fruit", "numberOfSeeds");
+    }
+
+    @Test
+    public void testVegetables() throws Exception {
+        checkForResponse("/Vegetables", "cabbage");
+    }
+
+    @Test
+    public void testVegetable() throws Exception {
+        checkForResponse("/Vegetable('brusselsprout')", "green");
     }
 }
